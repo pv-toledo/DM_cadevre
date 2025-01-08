1d449ebe7b42e339863d9b83d6715b54fc13e22b
Paulo Vinícius Vieira Toledo <pvvtoledo@gmail.com>
2025-01-08 16:01:45 -0300

    adicionado script para o cadastro.html, formatando os inputs de data de nascimento e telefone

---
+++ cadastro.js
@@ -0,0 +1,36 @@
+let dataNascimento = document.querySelector('#data_nasc');
+
+
+
+dataNascimento.addEventListener('input', function (e) {
+
+
+    let dnasc = dataNascimento.value.replace(/\D/g, ''); //essencial para retirar a barra que a formatação coloca, para que os condicionais sejam executados
+
+    if (e.inputType === 'deleteContentBackward') {
+        return;
+    } //se o usuário apagar com backspace o return interrompe a função (impede que o restante do código seja executado), permitindo que o usuário apague a data sem problemas com a formatação ser mantida
+
+    if (dnasc.length <= 2) {
+        dataNascimento.value = dnasc;
+    } else if (dnasc.length <= 3) {
+        dataNascimento.value = `${dnasc.slice(0, 2)}/${dnasc.slice(2)}`;
+    } else {
+        dataNascimento.value = `${dnasc.slice(0, 2)}/${dnasc.slice(2, 4)}/${dnasc.slice(4, 8)}`
+    }
+})
+
+let celNumero = document.querySelector('#tel');
+
+celNumero.addEventListener('input', function (e) {
+
+    let num = celNumero.value.replace(/\(|\)/g ,'')
+
+    if (e.inputType === 'deleteContentBackward') {
+        return;
+    }
+
+    if (num.length <= 2) {
+        celNumero.value = `(${num.slice(0,2)})`;
+    }
+})
